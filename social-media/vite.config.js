7ba49a9c3f753e06094db38780479dc6ac866702
Samarth Patel <samarth.patel1408@gmail.com>
2024-03-28 00:04:21 -0400

    vite config updated

--- social-media/vite.config.js
+++ social-media/vite.config.js
@@ -3,10 +3,9 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   server: {
-    proxy: { "/api": "http://localhost:4400" },
+    proxy: { "/api": "https://blogify-vp1v.onrender.com" },
   },
   plugins: [react()],
 });
