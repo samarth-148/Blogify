3af336cfe2a393650751ebc6d55115b1936b5292
Samarth Patel <samarth.patel1408@gmail.com>
2024-03-31 18:50:05 -0400

    auth updated

--- social-media-backend/controllers/user.controllers.js
+++ social-media-backend/controllers/user.controllers.js
@@ -15,7 +15,6 @@ async function handleLogin(req, res) {
     secure: true,
     expires: new Date(Date.now() + 30 * 24 * 60 * 60 * 1000), // Expires in 30 days
     sameSite: "None",
-    domain: ".netlify.app", // Set domain to .netlify.app
     path: "/",
   });
 
